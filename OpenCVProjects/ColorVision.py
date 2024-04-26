200dcb4140663bf13624fe764ae4686279d21a21
Neplooo <albertmathisz0@gmail.com>
2024-04-25 22:26:52 -0400

    Code Encapsulation Complete; Working on Test code in progress :)

--- OpenCVProjects/ColorVision.py
+++ OpenCVProjects/ColorVision.py
@@ -2,96 +2,82 @@
 import cv2
 import numpy as np
 
-# Create Variables
-vid = None
-
-def startCamera():
-    # Create Camera Object
-    global vid
-    vid = cv2.VideoCapture(0)
-    #vid.set(cv2.CAP_PROP_SATURATION, 65)
-
-    return vid
-
-def getFrame():
-    # Create a frame
-    ret, frame = vid.read()
-
-    # Convert RGB Values in frame to HSV
-    HSVConvert = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Red Limits: Lower: (0, 50, 50) Upper: (10, 255, 255)
-    # Green Limits: Lower: (40, 40, 40) Upper: (80, 255, 255)
-    # Blue Limits: Lower: (98, 50, 50) Upper: (139, 255, 255)
-
-    # Set the color mask limits
-    LLimit = (40, 40, 40)
-    ULimit = (80, 255, 255)
-
-    # Create Color Mask That isolates blue color from everything else
-    colorMask = cv2.inRange(HSVConvert, LLimit, ULimit)
-
-    color = cv2.bitwise_and(frame, frame, mask=colorMask)
-
-    # Create Contour line object
-    contours, hier = cv2.findContours(
-        colorMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-    )
-    # draw the contours
-    output = cv2.drawContours(color, contours, -1, (0, 0, 255), 3)
-    
-    # Create a mean filter that turns object into a blob: https://docs.opencv.org/3.4/d4/d13/tutorial_py_filtering.html
-    kernel = np.ones((5,5),np.float32)/25
-    meanFilter = cv2.filter2D(colorMask, -1, kernel)
-
-    # Gather Info about The frame
-    M = cv2.moments(meanFilter)
-    #print(M)
-    
-    # Take the Centroid Length and Width by doing fancy math: https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/ 
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    
-    #print(str(cX) + " " + str(cY))
-    
-    # Create Basic Robot Commands
-    if cX >= 350:
-        turnDir = "Left"
-    elif cX <= 250:
-        turnDir = "Right"
-    else:
-        turnDir = "Center"
-    
-    # Make a point for the centroid and place coordinates
-    cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
-    cv2.putText(frame, "Centroid: " + str(cX) + " " + str(cY) + " " + str(turnDir), (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2) # This Is format (:Centroid: :cX: :cY: :turnDir:)
-
-    # Show Frame
-    cv2.imshow("frame", frame)
-
-    # Show Isolated Color stuff
-    #cv2.imshow("Color Detector", color)
-    
-    # Show Mean Filter
-    #cv2.imshow("Mean Filter", meanFilter)
-
-    # Return the turnDir
-    return turnDir
-
-startCamera()
-
-while True:
-    getFrame()
-
-    # Quit with "Q" Button
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+class ColorDetection:
 
 
+    # Create Variables
+    def __init__(self):
+        vid = None
 
-# Kill windows after finished
-vid.release()
+    def startCamera():
+        # Create Camera Object
+        vid = cv2.VideoCapture(0)
+        #vid.set(cv2.CAP_PROP_SATURATION, 65)
 
-# Boom (Shaka Laka)
-cv2.destroyAllWindows()
+        return vid
+
+    def getFrame(vid):
+        # Create a frame
+        ret, frame = vid.read()
+
+        # Convert RGB Values in frame to HSV
+        HSVConvert = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Red Limits: Lower: (0, 50, 50) Upper: (10, 255, 255)
+        # Green Limits: Lower: (40, 40, 40) Upper: (80, 255, 255)
+        # Blue Limits: Lower: (98, 50, 50) Upper: (139, 255, 255)
+
+        # Set the color mask limits
+        LLimit = (40, 40, 40)
+        ULimit = (80, 255, 255)
+
+        # Create Color Mask That isolates blue color from everything else
+        colorMask = cv2.inRange(HSVConvert, LLimit, ULimit)
+
+        color = cv2.bitwise_and(frame, frame, mask=colorMask)
+
+        # Create Contour line object
+        contours, hier = cv2.findContours(
+            colorMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        # draw the contours
+        output = cv2.drawContours(color, contours, -1, (0, 0, 255), 3)
+        
+        # Create a mean filter that turns object into a blob: https://docs.opencv.org/3.4/d4/d13/tutorial_py_filtering.html
+        kernel = np.ones((5,5),np.float32)/25
+        meanFilter = cv2.filter2D(colorMask, -1, kernel)
+
+        # Gather Info about The frame
+        M = cv2.moments(meanFilter)
+        #print(M)
+        
+        # Take the Centroid Length and Width by doing fancy math: https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/ 
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        
+        #print(str(cX) + " " + str(cY))
+        
+        # Create Basic Robot Commands
+        if cX >= 350:
+            turnDir = "Left"
+        elif cX <= 250:
+            turnDir = "Right"
+        else:
+            turnDir = "Center"
+        
+        # Make a point for the centroid and place coordinates
+        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+        cv2.putText(frame, "Centroid: " + str(cX) + " " + str(cY) + " " + str(turnDir), (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2) # This Is format (:Centroid: :cX: :cY: :turnDir:)
+
+        # Show Frame
+        cv2.imshow("frame", frame)
+
+        # Show Isolated Color stuff
+        #cv2.imshow("Color Detector", color)
+        
+        # Show Mean Filter
+        #cv2.imshow("Mean Filter", meanFilter)
+
+        # Return the turnDir
+        return turnDir
 
--- tests/testCamera.py
+++ tests/testCamera.py
@@ -1,5 +1,30 @@
-import unitttest
-import opencv-python as cv2
+# NOTE: DO NOT USE, TEST ALGORITHM IS STILL BROKEN
+
+# Import Stuff
+import os, sys
+import unittest
+import cv2
 import numpy as np
 
-from ColorVision.py import
+#Find The Relative Path for The ColorVision.py file
+scriptDir = os.path.dirname(__file__)
+moduleDir = os.path.join(scriptDir, "..", "OpenCVProjects",)
+sys.path.append(moduleDir)
+
+# Import the ColorVision Code
+from ColorVision import ColorDetection
+
+#result = str(ColorDetection.startCamera())
+#print(result)
+#formatResult = result.join(line.strip() for line in result.splitlines())
+#print(formatResult)
+
+# CAMERA TEST STILL BROKEN
+class TestCamera(unittest.TestCase):
+    
+    def test_cam(self):
+        self.camCheck = ColorDetection()
+        cam = self.camCheck.startCamera
+        self.assertTrue()
+
+unittest.main(argv=[''], verbosity=2, exit=False)
